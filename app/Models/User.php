50f01811e8cdeb5893d40d4739aff1c482cc569b
John Puddephatt <john@jdp.org.uk>
2021-04-17 15:20:13 +0100

    add verification to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements FilamentUser
     use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use MustVerifyEmail;
 
 
     public static $filamentUserColumn = 'is_admin';
