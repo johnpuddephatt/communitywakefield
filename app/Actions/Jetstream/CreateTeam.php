78b7870a84641fe28a918ef054f71b1ff22f6ba6
John Puddephatt <john@jdp.org.uk>
2021-06-16 16:32:49 +0100

    null coelesce

--- app/Actions/Jetstream/CreateTeam.php
+++ app/Actions/Jetstream/CreateTeam.php
@@ -24,38 +24,38 @@ class CreateTeam implements CreatesTeams
      */
     public function create($user, array $input)
     {
-
-        Gate::forUser($user)->authorize('create', Jetstream::newTeamModel());
+        Gate::forUser($user)->authorize("create", Jetstream::newTeamModel());
 
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'logo' => ['nullable'],
-            'website' => ['nullable', 'string'],
-            'phone' => ['nullable', 'string'],
-            'email' => ['nullable', 'string'],
-            'auto_join' => ['nullable', 'string'],
-            'info' => ['nullable', 'string:500'],
+            "name" => ["required", "string", "max:255"],
+            "logo" => ["nullable"],
+            "website" => ["nullable", "string"],
+            "phone" => ["nullable", "string"],
+            "email" => ["nullable", "string"],
+            "auto_join" => ["nullable", "string"],
+            "info" => ["nullable", "string:500"],
+        ])->validateWithBag("createTeam");
 
-        ])->validateWithBag('createTeam');
-
-        if(isset($input['logo'])) {
-            $logo = Image::make($input['logo'])->resize(250, 250)->encode('png', 80);
-            $logo_name = Str::random(12) . '.png';
-            Storage::disk('public')->put($logo_name, $logo);
-            $logo_path = Storage::disk('public')->url($logo_name);
+        if (isset($input["logo"])) {
+            $logo = Image::make($input["logo"])
+                ->resize(250, 250)
+                ->encode("png", 80);
+            $logo_name = Str::random(12) . ".png";
+            Storage::disk("public")->put($logo_name, $logo);
+            $logo_path = Storage::disk("public")->url($logo_name);
         }
 
         AddingTeam::dispatch($user);
 
         $team = $user->ownedTeams()->create([
-            'name' => $input['name'],
-            'logo' => $logo_path,
-            'website' => $input['website'],
-            'phone' => $input['phone'],
-            'email' => $input['email'],
-            'auto_join' => $input['auto_join'],
-            'info' => $input['info'],
-            'personal_team' => false,
+            "name" => $input["name"],
+            "logo" => $logo_path ?? null,
+            "website" => $input["website"],
+            "phone" => $input["phone"],
+            "email" => $input["email"],
+            "auto_join" => $input["auto_join"],
+            "info" => $input["info"],
+            "personal_team" => false,
         ]);
 
         \Auth::user()->switchTeam($team);
